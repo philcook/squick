913be819b2ecc99449c9ab52907233946203ef55
Phil Cook <pcook@clickthrough-marketing.com>
2015-05-22 17:26:55 +0100

    Improvements of PSR-2 compliance and adding plugin install and theme activation.

--- command.php
+++ command.php
@@ -2,9 +2,9 @@
 /**
  * Squick WordPress helpers.
  */
-class Squick_Command extends WP_CLI_Command {
 
-
+class SquickCommand extends WP_CLI_Command
+{
     /**
     * Squickly installs WordPress using config from wp-cli.local.yml and wp-cli.yml
     *
@@ -14,7 +14,8 @@ class Squick_Command extends WP_CLI_Command {
     *
     * @when before_wp_load
     */
-    public function install($args, $assoc_args) {
+    public function install($args, $assoc_args)
+    {
         // rescue directory
         $rescue_directory = getcwd();
 
@@ -25,19 +26,28 @@ class Squick_Command extends WP_CLI_Command {
         $extra_config = \WP_CLI::get_runner()->extra_config;
 
         // 1) download ...
-        WP_CLI::run_command( array('core', 'download'));
+        WP_CLI::run_command(array('core', 'download'));
 
         // 2) create database ...
         $db_config = $extra_config['core config'];
-        if(empty($db_config['dbhost'])) {
+        if (empty($db_config['dbhost'])) {
             $db_config['dbhost'] = '127.0.0.1';
         }
         $mysqli = new mysqli($db_config['dbhost'], $db_config['dbuser'], $db_config['dbpass']);
-        if( ! $mysqli) {
-            WP_CLI::error(sprintf("Unable to connect to database '%s' with user '%s'.", $db_config['dbhost'], $db_config['dbuser']));
+        if (! $mysqli) {
+            WP_CLI::error(
+                sprintf(
+                    "Unable to connect to database '%s' with user '%s'.",
+                    $db_config['dbhost'],
+                    $db_config['dbuser']
+                )
+            );
             return;
         }
-        if( ! $mysqli->query("CREATE DATABASE IF NOT EXISTS `" . $db_config['dbname']. "` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;")) {
+
+        $sql = "CREATE DATABASE IF NOT EXISTS `".$db_config['dbname'].
+            "` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;";
+        if (! $mysqli->query($sql)) {
             WP_CLI::error(sprintf("Unable to create new schema '%s'.", $db_config['dbname']));
             return;
         }
@@ -48,25 +58,58 @@ class Squick_Command extends WP_CLI_Command {
         WP_CLI::run_command(array('core', 'config'));
 
         // 4) install ..
-        WP_CLI::run_command(array('core', 'install'));
-        WP_CLI::success("Finished core install.");
+        echo exec('wp core install')."\n";
 
-        // 5) plugin install
-        WP_CLI::line('Installing list of plugins...');
-        foreach ($extra_config['plugin install'] as $plugin) {
-            WP_CLI::run_command( array('plugin', 'install' , $plugin));
-            WP_CLI::run_command( array('plugin', 'activate' , $plugin));
-            WP_CLI::success(sprintf("Finished installing %s plugin",$plugin));
+        // 5) Remove default plugins and themes
+        $dirs = array($config['path'] . 'wp-content/plugins/', $config['path'] . 'wp-content/themes/');
+        $leave_items = array('index.php', $extra_config['theme-name']);
+        foreach ($dirs as $dir) {
+            if (is_dir($dir)) {
+                foreach (glob("$dir*") as $item) {
+                    if (!in_array(basename($item), $leave_items)) {
+                        if (is_dir($item)) {
+                            $this->rrmdir($item);
+                        } else {
+                            unlink($item);
+                        }
+                    }
+                }
+            }
         }
 
-        // rescue directory
+        // 6) plugin install
+        WP_CLI::line('Installing list of plugins...');
+        foreach ($extra_config['plugin install'] as $plugin) {
+            echo exec('wp plugin install '.$plugin. ' --activate')."\n";
+        }
+
+        // 7) Activate theme
+        echo exec('wp theme activate '.$extra_config['theme-name'])."\n";
+
         chdir($rescue_directory);
 
-        if(isset($config['url'])) {
+        if (isset($config['url'])) {
             WP_CLI::launch('open ' . $config['url']);
         }
     }
 
+    private function rrmdir($dir)
+    {
+        if (is_dir($dir)) {
+            $objects = scandir($dir);
+            foreach ($objects as $object) {
+                if ($object != "." && $object != "..") {
+                    if (filetype($dir."/".$object) == "dir") {
+                        $this->rrmdir($dir."/".$object);
+                    } else {
+                        unlink($dir."/".$object);
+                    }
+                }
+            }
+            reset($objects);
+            rmdir($dir);
+        }
+    }
 }
 
-WP_CLI::add_command( 'squick', 'Squick_Command' );
+WP_CLI::add_command('squick', 'SquickCommand');
